b7187b46b31aef261ca47dc04a48edee580b0fc0
ahmdelemam <ahmdelemam@gmail.com>
2019-04-16 15:30:21 +0200

    DB-migration: Create TodoItems

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_04_16_120256) do
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string "title"
+    t.boolean "read"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
